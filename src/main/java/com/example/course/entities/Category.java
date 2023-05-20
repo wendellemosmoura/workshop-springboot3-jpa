ec73726c0b7d192926c52359f6520ce740022c81
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2023-05-20 20:59:01 -0300

    Category

---
+++ src/main/java/com/example/course/entities/Category.java
@@ -0,0 +1,22 @@
+package com.example.course.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
